11f19228ba54acd29ec367a46b5ca54ff74f1963
jemeka2 <jemeka2@oberlin.edu>
2021-08-04 09:29:34 -0400

    Final template

--- src/main/java/com/example/springboot_security_bookedition/UserRepo.java
+++ src/main/java/com/example/springboot_security_bookedition/UserRepo.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 //CRUD - Create, read, update, delete
 public interface UserRepo extends CrudRepository<User, Long> {
+    User findByUsername(String username);
 }
